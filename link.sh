79c17c6fe7fd7ac487e1c5855f6f79b87efee2cd
Taylor Lodge <taylor@boost.co.nz>
2015-10-12 09:14:58 +1300

    Fix tmux crashing on non OSX systems

--- link.sh
+++ link.sh
@@ -1,9 +1,9 @@
 directory=`dirname $0`
 
-echo "ln -fs ${directory}/vimrc ~/.vimrc"
 ln -fs ${directory}/vimrc ~/.vimrc
 ln -fs ${directory}/gvimrc ~/.gvimrc
 ln -fs ${directory}/rspec ~/.rspec
 ln -fs ${directory}/zshrc ~/.zshrc
 ln -fs ${directory}/tmux.conf ~/.tmux.conf
 ln -fs ${directory}/nvimrc ~/.nvimrc
+ln -fs ${directory}/tmux.osx.conf ~/.tmux.osx.conf
