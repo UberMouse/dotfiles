dc9cb1eea90167ec73a55df233d0c593c89c66f3
Taylor Lodge <ubermouse894@gmail.com>
2017-03-13 13:35:00 +1300

    Install fzf to correct plugin location & install neovim and ag

--- bootstrap_neovim.sh
+++ bootstrap_neovim.sh
@@ -1,3 +1,4 @@
-curl -fLo ~/.nvim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+brew install neovim/neovim/neovim
+brew install ag
