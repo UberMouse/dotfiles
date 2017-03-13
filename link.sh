da662942b3bd5e431be8005f476bcad9f17f407c
Taylor Lodge <ubermouse894@gmail.com>
2017-03-13 13:34:30 +1300

    KWM and KHD RCs

--- link.sh
+++ link.sh
@@ -3,5 +3,8 @@ directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -fs ${directory}/rspec ~/.rspec
 ln -fs ${directory}/zshrc ~/.zshrc
 ln -fs ${directory}/tmux.conf ~/.tmux.conf
+ln -fs ${directory}/khdrc ~/.khdrc
+mkdir -p ~/.kwm
+ln -fs ${directory}/kwmrc ~/.kwm/kwmrc
 mkdir -p ~/.config/nvim
 ln -fs ${directory}/nvimrc ~/.config/nvim/init.vim
