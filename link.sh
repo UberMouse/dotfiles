7eb26352edf08ba647b2470a2e8340e78764d0fc
Taylor Lodge <ubermouse894@gmail.com>
2019-08-26 14:17:38 +1200

    add skhdrc and chunkwmrc & update link.sh

--- link.sh
+++ link.sh
@@ -3,5 +3,7 @@ directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -fs ${directory}/rspec ~/.rspec
 ln -fs ${directory}/zshrc ~/.zshrc
 ln -fs ${directory}/tmux.conf ~/.tmux.conf
+ln -fs ${directory}/skhdrc ~/.skhdrc
+ln -fs ${directory}/chunkwmrc ~/.chunkwmrc
 mkdir -p ~/.config/nvim
 ln -fs ${directory}/nvimrc ~/.config/nvim/init.vim
