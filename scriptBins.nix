12290b61d5c656c4b344e06968a28ce9eeff0258
Taylor Lodge <taylor.lodge@koordinates.com>
2023-11-14 08:54:05 +1300

    fix koordinates-dev-protocol script

--- scriptBins.nix
+++ scriptBins.nix
@@ -4,7 +4,7 @@
   home.packages = [
     (pkgs.writeScriptBin "koordinates-dev-protocol" ''
       #!/usr/bin/env bash
-      curl -X POST -H 'Content-Type: application/json' -d '{"url": "$1"}' http://localhost:7281
+      curl -X POST -H 'Content-Type: application/json' -d "{\"url\": \"$1\"}" http://localhost:7281
     '')
   ]; 
 }
