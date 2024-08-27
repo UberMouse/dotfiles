6501a4364733600fb0a22e6a04f295156ff6543e
Taylor Lodge <taylor.lodge@koordinates.com>
2024-08-28 09:40:59 +1200

    enable tailscale

--- nixos.nix
+++ nixos.nix
@@ -123,6 +123,8 @@
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "taylorl";
+  
+  services.tailscale.enable = true;
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
