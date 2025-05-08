9a5ae11ae82249e05945f236dc1da356a7cd75f5
Taylor Lodge <taylor.lodge@koordinates.com>
2025-05-09 09:14:35 +1200

    update playwright & lock update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     playwright = {
-      url = "github:pietdevries94/playwright-web-flake/1.51.0";
+      url = "github:pietdevries94/playwright-web-flake/1.52.0";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     home-manager = {
